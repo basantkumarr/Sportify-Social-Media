91082c616f171cec7183ae30b17cece2c725754d
Basant Kumar <119030323+basantkumarr@users.noreply.github.com>
2024-07-22 15:16:41 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,7 +7,6 @@ import userRoutes from './Routes/userRoute.js';
 import postRoutes from './Routes/postRoute.js';
 import path from 'path';
 
- 
 dotenv.config({ path: '.env' });
 
 const app = express();
@@ -20,15 +19,21 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 // Configure CORS
-app.use(cors({
+const corsOptions = {
   origin: 'https://sportify-chi.vercel.app', // Replace with your frontend origin
-  credentials: true
-}));
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Specify allowed methods
+  allowedHeaders: ['Content-Type', 'Authorization'], // Specify allowed headers
+  optionsSuccessStatus: 200, // Some legacy browsers choke on 204
+};
 
+app.use(cors(corsOptions));
+
+// Middleware to handle preflight requests for all routes
+app.options('*', cors(corsOptions));
 
 const __dirname = path.resolve();
 app.use('/images', express.static(path.join(__dirname, 'public/images')));
-
 
 // Connect to MongoDB
 mongoose.connect(MONGO_URI)
@@ -37,10 +42,9 @@ mongoose.connect(MONGO_URI)
 
 // Routes
 app.use("/user", userRoutes);
- app.use("/post",postRoutes)
+app.use("/post", postRoutes);
 
 // Start server
-
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
