b4bb57f6e2d89efced8b96c171dcd906e4812f4d
Basant Kumar <119030323+basantkumarr@users.noreply.github.com>
2024-07-22 15:19:05 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,44 +7,36 @@ import userRoutes from './Routes/userRoute.js';
 import postRoutes from './Routes/postRoute.js';
 import path from 'path';
 
-dotenv.config({ path: '.env' });
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 const MONGO_URI = process.env.MONGO_URI;
 
-// Middleware to parse JSON bodies and cookies
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-// Configure CORS
 const corsOptions = {
-  origin: 'https://sportify-chi.vercel.app', // Replace with your frontend origin
+  origin: 'https://sportify-chi.vercel.app',
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Specify allowed methods
-  allowedHeaders: ['Content-Type', 'Authorization'], // Specify allowed headers
-  optionsSuccessStatus: 200, // Some legacy browsers choke on 204
 };
 
 app.use(cors(corsOptions));
 
-// Middleware to handle preflight requests for all routes
+// Middleware to handle preflight requests
 app.options('*', cors(corsOptions));
 
 const __dirname = path.resolve();
 app.use('/images', express.static(path.join(__dirname, 'public/images')));
 
-// Connect to MongoDB
 mongoose.connect(MONGO_URI)
   .then(() => console.log('MongoDB connected...'))
   .catch(err => console.error('MongoDB connection error:', err.message));
 
-// Routes
 app.use("/user", userRoutes);
 app.use("/post", postRoutes);
 
-// Start server
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
