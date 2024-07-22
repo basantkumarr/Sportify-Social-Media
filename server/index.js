4030e15dfa96a33520505642edb4048021b22507
Basant Kumar <119030323+basantkumarr@users.noreply.github.com>
2024-07-22 15:10:39 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ app.use(cookieParser());
 
 // Configure CORS
 app.use(cors({
-  origin: 'http://localhost:5173', // Replace with your frontend origin
+  origin: 'https://sportify-chi.vercel.app', // Replace with your frontend origin
   credentials: true
 }));
 
