6be09ddd7b96aa843c7ba9d063be8e486cf14e22
Basant Kumar <119030323+basantkumarr@users.noreply.github.com>
2024-07-23 08:13:33 +0530

    Update Mid.jsx

--- my-project/src/pages/Mid.jsx
+++ my-project/src/pages/Mid.jsx
@@ -1,14 +1,28 @@
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import Fed from './Fed';
 import Tweet from './Tweet';
+import useGetMyPost from '../hooks/useGetMyPost';
 
 const Mid = () => {
+  const dispatch = useDispatch();
   const { posts } = useSelector((store) => store.post);
+  const userId = useSelector((state) => state.user.user?.id); // Adjust according to your state structure
 
   console.log("Posts in Mid component:", posts); // Debugging log
 
+  // Use custom hook to fetch posts
+  useGetMyPost(userId);
+
+  useEffect(() => {
+    // Fetch posts when component mounts or userId changes
+    if (userId) {
+      dispatch(toggleRefresh()); // Trigger refresh if needed
+    }
+  }, [userId, dispatch]);
+
   return (
-    <div className='lg:w-[60%] sm:w-[100%]  border border-gray-200 h-screen overflow-y-auto custom-scrollbar'>
+    <div className='lg:w-[60%] sm:w-[100%] border border-gray-200 h-screen overflow-y-auto custom-scrollbar'>
       <div>
         <Fed />
         {posts && posts.length > 0 ? (
@@ -22,3 +36,4 @@ const Mid = () => {
 };
 
 export default Mid;
+
